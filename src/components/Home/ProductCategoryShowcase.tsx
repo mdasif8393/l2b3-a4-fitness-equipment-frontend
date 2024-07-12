a5c49d3c0e8934d3684ab937c819c9efce95ea91
mdasif8393 <md.asif.8393@gmail.com>
2024-07-12 15:34:25 +0600

    add hero section

---
+++ src/components/Home/ProductCategoryShowcase.tsx
@@ -0,0 +1,5 @@
+const ProductCategoryShowcase = () => {
+  return <div></div>;
+};
+
+export default ProductCategoryShowcase;
--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -9,8 +9,18 @@ import {
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center bg-gray-900 h-16">
       <NavigationMenu>
+        <Link to="/">
+          <div className="flex items-center justify-center mx-2">
+            <img
+              src="https://img.freepik.com/premium-vector/fitness-workout-weight-sport-gym-tools-exercise-equipment-vector-image-gym-logo-fitness-tools_445443-47.jpg"
+              alt=""
+              className="w-14"
+            />
+            <h4 className="text-xl font-bold text-white ml-2">Gym Tools</h4>
+          </div>
+        </Link>
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
